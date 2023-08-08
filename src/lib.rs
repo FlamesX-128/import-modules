66cd1a0462975013691940e012322957e0978c23
FlamesX-128 <flamesx128@gmail.com>
2023-08-08 04:08:30 -0600

    something

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,107 @@
-use modules::{lexical, syntactic};
+use proc_macro::TokenStream;
 
-mod models;
-mod modules;
+#[derive(Clone, Debug)]
+struct Module {
+    pub submodules: Vec<Module>,
+    pub name: String,
+}
 
-extern crate proc_macro;
+impl Module {
+    pub fn new() -> Self {
+        Self {
+            submodules: Vec::new(),
+            name: String::new(),
+        }
+    }
+}
+
+fn parse_modules(input: &str) -> Module {
+    let stack = &mut Vec::new();
+
+    let mut module = Module::new();
+
+    for c in input.chars() {
+        match c {
+            '{' => {
+                stack.push(module);
+                module = Module::new();
+            }
+            '}' => {
+                let mut parent = stack.pop().unwrap();
+                parent.submodules.push(module);
+
+                module = parent;
+            }
+            ',' => {
+                let parent = stack.last_mut().unwrap();
+                parent.submodules.push(module);
+
+                module = Module::new();
+            }
+            '&' => {
+                //stack.push(module.clone());
+
+                let mut parent = stack.pop().unwrap();
+                parent.submodules.push(module);
+
+                module = parent;
+            }
+            _ => {
+                module.name.push(c);
+            }
+        }
+    }
+
+    module
+}
+
+fn generate_import(path: String, module: Module) -> Vec<String> {
+    let mut current = String::new();
+    current.push_str(&path);
+    current.push_str(&module.name);
+
+    let mut data = Vec::new();
+    data.push(current.clone());
+
+    for submodule in module.submodules {
+        data.append(
+            &mut generate_import(current.clone() + "::", submodule)
+        );
+    }
+
+   data 
+}
 
 #[proc_macro]
-pub fn import(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let document = input.to_string();
-    let chars = document.chars();
+pub fn import(input: TokenStream) -> TokenStream {
+    let input = input.to_string();
+    
+    let args = input.trim().split_whitespace();
 
-    let mut scope = lexical::Scope::new(chars);
-    let identifiers = lexical::lexer(&mut scope);
+    let (prefix, path) = match args.clone().nth(0).unwrap_or_default() {
+        "crate" | "mod" | "self" | "use" => {
+            let prefix = args.clone().nth(0).unwrap_or_default();
+            let path = args.clone().skip(1).collect::<Vec<_>>().join("");
 
-    let mut scope = syntactic::Scope::new(&document, identifiers);
-    let result = syntactic::parser(&mut scope);
+            (prefix, path)
+        }
+        _ => {
+            let path = args.clone().collect::<Vec<_>>().join("");
 
-    println!("{:?}", result);
+            ("", path)
+        }
+    };
+
+    let path = path.replace("::", "");
+    
+    let module = parse_modules(&path);
+    let imports = generate_import(String::new(), module.clone());
+
+    println!("prefix: {}", prefix);
+    println!("path: {}", path);
+    println!("path: {:#?}", module);
+    println!("{:#?}", imports);
 
     "".parse().unwrap()
 }
+
--- tests/mod.rs
+++ tests/mod.rs
@@ -1,5 +1,26 @@
-use import_modules::import;
+/*use import_modules::import;
 
 import!(
     "tests/" where "" use as mod
-);
+);*/
+
+use import_modules::import;
+
+#[test]
+fn test() {
+    /*let current_file = file!();
+    let current_directory = env!("PWD"); // This will retrieve the current working directory
+
+    println!("Current File: {}", current_file);
+    println!("Current Directory: {}", current_directory);*/
+
+    // {module1{module2{module3,module4{module5,module6}},module7}}
+    /*import!(
+        {module1{module2{module3,module4{module5,module6}},module7}}
+    );*/
+
+    import!(
+        use models::{a::{*}, b::{*}}
+    );
+}
+
